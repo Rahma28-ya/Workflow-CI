ba69c4c466082f004dc47f9f8130f37b2112a678
Rahma Vadilla <rahmavadilla28@gmail.com>
2025-12-19 19:59:26 +0700

    Fix MLflow Project CI and docker workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,15 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
-# 1. Load dataset hasil preprocessing
 DATA_PATH = "namadataset_preprocessing/telco_customer_churn_preprocessing.csv"
 df = pd.read_csv(DATA_PATH)
 
-# Target diasumsikan kolom terakhir
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 
-# 2. Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
@@ -22,21 +19,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-# 3. Set experiment MLflow
 mlflow.set_experiment("Telco_Customer_Churn")
 
-# Aktifkan autolog
-mlflow.sklearn.autolog()
-
-# 4. Training model
-with mlflow.start_run(run_name="LogisticRegression_Basic"):
+with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
-    # 5. Evaluasi
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
     print("Accuracy:", acc)
-    print("\nClassification Report:")
     print(classification_report(y_test, y_pred))
