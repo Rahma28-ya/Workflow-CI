347f2f881cce7a5a942b1a8e8b383b0da4e489fc
Rahma Vadilla <rahmavadilla28@gmail.com>
2025-12-19 20:11:32 +0700

    Fix missing LogisticRegression import

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,15 +21,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.set_experiment("Telco_Customer_Churn")
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.log_model(model, "model")
 
-    print("Accuracy:", acc)
-    print(classification_report(y_test, y_pred))
+print("Accuracy:", acc)
+print(classification_report(y_test, y_pred))
