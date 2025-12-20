80275e022fb1c8d83de6110afb2cfa2d5a68844e
Rahma Vadilla <rahmavadilla28@gmail.com>
2025-12-20 15:57:07 +0700

    Final version CI with MLflow and Docker

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,18 +1,26 @@
+import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import os
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
+# Path dataset
 DATA_PATH = "namadataset_preprocessing/telco_customer_churn_preprocessing.csv"
+# Folder model untuk MLflow
+MODEL_DIR = "MLProject/model"
+
+# Pastikan folder model ada
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+# Load data
 df = pd.read_csv(DATA_PATH)
 
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
@@ -20,20 +28,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
+# Training model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
+# Evaluasi
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
-# LANGSUNG LOG
+# Log metric dan model ke MLflow
 mlflow.log_metric("accuracy", acc)
-mlflow.sklearn.log_model(model, artifact_path="model")
-
-# SAVE MODEL KE FOLDER FISIK UNTUK DOCKER
-MODEL_DIR = "MLProject/model"
-os.makedirs(MODEL_DIR, exist_ok=True)
-mlflow.sklearn.save_model(model, MODEL_DIR)
+mlflow.sklearn.log_model(model, artifact_path=MODEL_DIR)
 
 print("Accuracy:", acc)
 print(classification_report(y_test, y_pred))
